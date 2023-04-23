ec276db13fba67557a7410d39bae2e2c5083ceac
prashanta0234 <prashanta0234@gmail.com>
2023-04-24 04:11:00 +0600

    add some get request

---
+++ src/app.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+import { Products as productModel } from '@prisma/client';
+import { ProductService } from './product.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly productsService: ProductService) {}
+
+  // get all products
+  @Get('products')
+  async getProducts(): Promise<productModel[]> {
+    return this.productsService.products();
+  }
+
+  // get single product by id
+  @Get('products/:id')
+  async getProductById(@Param('id') id: number): Promise<productModel> {
+    return this.productsService.product({ id: Number(id) });
+  }
+
+  // get Product by category
+  @Get(':category')
+  async getCategory(
+    @Param('category') category: string,
+  ): Promise<productModel[] | productModel> {
+    return this.productsService.productByCategory({
+      category: { has: category },
+    });
+  }
+
+  // sending fine status
+  @Get()
+  async hello(): Promise<string> {
+    return 'hi i am running fine!';
+  }
+}
---
+++ src/product.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { Prisma, Products } from '@prisma/client';
+
+@Injectable()
+export class ProductService {
+  static products: Products[] | PromiseLike<Products[]>;
+  constructor(private prisma: PrismaService) {}
+
+  // find all products
+  async products(): Promise<Products[]> {
+    return this.prisma.products.findMany();
+  }
+
+  // product by id
+  async product(id: Prisma.ProductsWhereUniqueInput): Promise<Products> {
+    return this.prisma.products.findUnique({
+      where: id,
+    });
+  }
+
+  // products by category
+  async productByCategory(
+    category: Prisma.ProductsWhereInput,
+  ): Promise<Products[]> {
+    return this.prisma.products.findMany({
+      where: category,
+    });
+  }
+}
